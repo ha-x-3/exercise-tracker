ab01ac829c3147e0d207300d60427c8e079291de
Brittney Epperson <beppers87@hotmail.com>
2024-08-01 19:23:55 +0000

    Added routes for users, exercises, and logs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-//Import models
+// Import models
 const User = require('./models/user');
 const Exercise = require('./models/exercise');
 
@@ -22,16 +22,110 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-//Routes
-//Home
+// Routes
+// Home
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// POST /api/users
+app.post('/api/users', async (req, res, next) => {
+  try {
+    const user = new User({ username: req.body.username });
+    const savedUser = await user.save();
+    res.json({
+      username: savedUser.username,
+      _id: savedUser._id
+    });
+  } catch (err) {
+    if (err.code === 11000) {
+      return next({
+        status: 400,
+        message: 'Username already taken'
+      });
+    }
+    next(err);
+  }
+});
 
+// POST /api/users/:_id/exercises
+app.post('/api/users/:_id/exercises', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params._id);
+    if (!user) {
+      return next({
+        status: 400,
+        message: 'Unknown userId'
+      });
+    }
+    
+    const exercise = new Exercise({
+      userId: user._id,
+      username: user.username,
+      description: req.body.description,
+      duration: Number(req.body.duration),
+      date: req.body.date ? new Date(req.body.date) : new Date()
+    });
+    
+    const savedExercise = await exercise.save();
+    
+    res.json({
+      username: user.username,
+      description: savedExercise.description,
+      duration: savedExercise.duration,
+      date: savedExercise.date.toDateString(),
+      _id: user._id
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/users/:_id/logs
+app.get('/api/users/:_id/logs', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params._id);
+    if (!user) {
+      return next({
+        status: 400,
+        message: 'Unknown userId'
+      });
+    }
+    
+    let dateFilter = {};
+    if (req.query.from) {
+      dateFilter.$gte = new Date(req.query.from);
+    }
+    if (req.query.to) {
+      dateFilter.$lte = new Date(req.query.to);
+    }
+    
+    let limit = req.query.limit ? Number(req.query.limit) : undefined;
+    
+    let exercises = await Exercise.find({
+      userId: user._id,
+      ...(Object.keys(dateFilter).length && { date: dateFilter })
+    }).limit(limit).select('-_id description duration date');
+    
+    exercises = exercises.map(e => ({
+      description: e.description,
+      duration: e.duration,
+      date: e.date.toDateString()
+    }));
+    
+    res.json({
+      username: user.username,
+      count: exercises.length,
+      _id: user._id,
+      log: exercises
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
