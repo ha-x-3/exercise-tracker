b35e037e63ee42065885d3dde20965285e9f1ccd
Brittney Epperson <beppers87@hotmail.com>
2024-08-01 18:41:15 +0000

    Connected to MongoDB and added middleware

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.use(cors())
-app.use(express.static('public'))
+// Connect to MongoDB
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB is connected..."))
+  .catch((err) => console.log(err));
+
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
